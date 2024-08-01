4b5b6b916f49d4a6cbb5cfd75dd48eeab0217b98
samuloko <rjsamuel.opop@outlook.com>
2024-08-01 19:06:37 -0300

    começo

--- admin/static/index.php
+++ admin/static/index.php
@@ -1,22 +1,23 @@
 <?php
+//inicia uma nova sessão para o usuário, ou acaba com a existente
 session_start();
 unset($_SESSION);
 session_destroy();
 ?>
 <head>
+
+  <!------ chama as funções externas de css para a página ---------->
+
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<!------ Include the above in your HEAD tag ---------->
-
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-
-<!-- Include the above in your HEAD tag -->
-
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <style>
+
+/* colocando os parametros para a responsividade */
 
 @media (min-width:1100px){
   #login {
@@ -133,6 +134,7 @@ width: 100%;
       <div id="login">
 
       <?php
+      //verificação para mensagem de login inválido
       if(!empty($_GET['invalid']) && $_GET['invalid'] = 'true'){
         echo '	<div class="alert alert-danger alert-dismissible fade show" role="alert" style="font-size:20px">
                   Login inválido.
@@ -146,13 +148,13 @@ width: 100%;
           <div class="row">
           <div class="form-group col-md-12 col-sm-4">
 
-          <span class="fa fa-user"></span><input type="text"  Placeholder="Usuário" name="usuario" id="txUsuario" style="font-size:20px" required> <!-- JS because of IE support; better: placeholder="Username" -->
+          <span class="fa fa-user"></span><input type="text"  Placeholder="Usuário" name="usuario" id="txUsuario" style="font-size:20px" required>
           </div>
             </div>
             <div class="row">
             <div class="form-group col-md-12 col-sm-4">
 
-              <p><span class="fa fa-lock"></span><input type="password"  Placeholder="Senha" name="senha" id="txSenha" style="font-size:20px" required></p> <!-- JS because of IE support; better: placeholder="Password" -->
+              <p><span class="fa fa-lock"></span><input type="password"  Placeholder="Senha" name="senha" id="txSenha" style="font-size:20px" required></p>
               </div>
 
             </div>
@@ -168,7 +170,7 @@ width: 100%;
 
         <div class="clearfix"></div>
 
-      </div> <!-- end login -->
+      </div>
       <div class="logo"><img src="img/faetec.png" width="450" height="150" style="padding:25px" alt="">
           
           <div class="clearfix"></div>
